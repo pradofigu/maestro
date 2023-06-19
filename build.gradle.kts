bfca6324cb806c97897232af0c979959e29d5178
Priscila Carvalho <pri.carvalho86@gmail.com>
2023-06-19 15:22:07 -0600

    infrastructure: configure jooq entity generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jooq.meta.jaxb.ForcedType
+import org.jooq.meta.jaxb.Logging
+import org.jooq.meta.jaxb.Property
 
 plugins {
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.flywaydb.flyway") version "9.8.1"
+    id("nu.studer.jooq") version "8.2"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
 }
@@ -36,6 +40,7 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     runtimeOnly("org.postgresql:postgresql")
 //    runtimeOnly("org.postgresql:r2dbc-postgresql")
+    jooqGenerator("org.postgresql:postgresql:42.5.1")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
@@ -53,7 +58,20 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+sourceSets {
+    //add a flyway sourceSet
+    val flyway by creating {
+        compileClasspath += sourceSets.main.get().compileClasspath
+        runtimeClasspath += sourceSets.main.get().runtimeClasspath
+    }
+    //main sourceSet depends on the output of flyway sourceSet
+    main {
+        output.dir(flyway.output)
+    }
+}
+
 flyway {
+    driver=System.getenv("datasource.driver")
     url = System.getenv("datasource.jdbc-url")
     user = System.getenv("datasource.username")
     password = System.getenv("datasource.password")
@@ -62,4 +80,65 @@ flyway {
     defaultSchema = "flyway"
     schemas=arrayOf("flyway")
     locations = arrayOf("filesystem:src/main/resources/db/migration","classpath:db/migration")
+}
+
+jooq {
+//    version.set(dependencyManagement.importedProperties['jooq.version'])  // default (can be omitted)
+    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)  // default (can be omitted)
+
+    configurations {
+        create("main") {  // name of the jOOQ configuration
+            generateSchemaSourceOnCompilation.set(true)  // default (can be omitted)
+
+            jooqConfiguration.apply {
+                logging = Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/maestro"
+                    user = "admin"
+                    password = "admin"
+                    properties.add(Property().apply {
+                        key = "ssl"
+                        value = "false"
+                    })
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                        forcedTypes.addAll(listOf(
+                            ForcedType().apply {
+                                name = "varchar"
+                                includeExpression = ".*"
+                                includeTypes = "JSONB?"
+                            },
+                            ForcedType().apply {
+                                name = "varchar"
+                                includeExpression = ".*"
+                                includeTypes = "INET"
+                            }
+                        ))
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                    }
+                    target.apply {
+                        packageName = "br.com.pradofigo.maestro.infrastructure.entities"
+                        directory = "build/generated-sources/jooq/main"  // default (can be omitted)
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
+}
+
+tasks.named<nu.studer.gradle.jooq.JooqGenerate>("generateJooq") {
+    (launcher::set)(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    })
 }
