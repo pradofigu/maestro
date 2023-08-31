afff6f844a3ac15c4d071e69421e94ea26164ad3
Diogo Silva <diogosilvaoficial@gmail.com>
2023-08-31 00:37:49 -0300

    Add kuberbetes

---
+++ kubernetes/delete.sh
@@ -0,0 +1,5 @@
+kubectl delete pods --all
+kubectl delete deployments --all
+kubectl delete services --all
+kubectl delete configmaps --all
+kubectl delete secrets --all
---
+++ kubernetes/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Aplicar Secrets e Configmaps
+kubectl apply -f config/maestro-secrets.yaml
+kubectl apply -f config/maestro-configmap.yaml
+
+# Aplicar Services
+kubectl apply -f services/svc-postgres.yaml
+kubectl apply -f services/svc-maestro-app.yaml
+
+# Aplicar Pods
+kubectl apply -f database/postgres-pod.yaml
+kubectl apply -f maestro-app/maestro-app.yaml
